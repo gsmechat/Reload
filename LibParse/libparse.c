18f88bf6f771c82b7715caf223c5c94fa4663202
mechat_g <mechat_g@b3d5a8ed-ae99-4402-9d51-05b63b276f9b>
2016-02-10 18:27:34 +0000

    avancer jusquau parse d'argument

--- LibParse/libparse.c
+++ LibParse/libparse.c
@@ -5,18 +5,51 @@
 ** Login   <mechat_g@etna-alternance.net>
 ** 
 ** Started on  Tue Feb  9 01:52:03 2016 MECHAT Guillaume
-** Last update Tue Feb  9 02:11:02 2016 MECHAT Guillaume
+** Last update Tue Feb  9 04:31:48 2016 MECHAT Guillaume
 */
+#include <stdlib.h>
+
+char	*parcours(char *str)
+{
+  int	i;
+  int	j;
+  char	*prog;
+  char	*arg;
+
+  i = 0;
+  j = 0;
+  prog = malloc(sizeof(char));
+  arg = malloc(sizeof(char));
+  if (prog == 0 || arg == 0)
+    return (NULL);
+  while (str[i] != '\0' && str[i] != ':')
+    {
+      prog[i] = str[i];
+      i = i + 1;
+    }
+  while (str[i] != '\0')
+    {
+      if (j == 0)
+	i = i + 1;
+      arg[j] = str[i];
+      i = i + 1;
+      j = j + 1;
+    }
+  my_putstr("prog = ");
+  my_putstr(prog);
+  my_putstr("\narg = ");
+  my_putstr(arg);
+}
 
 void	libparse(int ac, char **av)
 {
   int	i;
-
+  
   i = 1;
   while (i < ac)
     {
-      if (
-      my_putstr(av[i]);
+      if (av[i][0] == '-')
+	parcours(av[i]);
       i = i + 1;
     }
 }
