fcb5475709e8b1968b591b54183888d09ed65980
mechat_g <mechat_g@b3d5a8ed-ae99-4402-9d51-05b63b276f9b>
2016-02-10 16:04:55 +0000

    ad modif

--- Reload/mechat_g/LibParse/main.c
+++ Reload/mechat_g/LibParse/main.c
@@ -5,11 +5,11 @@
 ** Login   <mechat_g@etna-alternance.net>
 ** 
 ** Started on  Mon Feb  8 20:40:04 2016 MECHAT Guillaume
-** Last update Tue Feb  9 01:32:21 2016 MECHAT Guillaume
+** Last update Tue Feb  9 01:51:41 2016 MECHAT Guillaume
 */
 
 int	main(int argc, char **argv)
 {
-  
+  libparse(argc, argv);
   return (0);
 }
