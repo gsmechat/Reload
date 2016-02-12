f6b3e74ae10f5555ca232da6c16ae76acd99f1fa
mechat_g <mechat_g@b3d5a8ed-ae99-4402-9d51-05b63b276f9b>
2016-02-12 10:05:34 +0000

    add modif sur makefille libparse et main ajout de structure t_parse

--- Reload/mechat_g/LibParse/libparse.c
+++ Reload/mechat_g/LibParse/libparse.c
@@ -5,23 +5,57 @@
 ** Login   <mechat_g@etna-alternance.net>
 ** 
 ** Started on  Tue Feb  9 01:52:03 2016 MECHAT Guillaume
-** Last update Tue Feb  9 04:54:47 2016 MECHAT Guillaume
+** Last update Fri Feb 12 10:34:33 2016 MECHAT Guillaume
 */
 #include <stdlib.h>
+#include "header.h"
 
-char	*parcours(char *str)
+t_parse		*add_parse(t_parse *parse, char *prog, char *arg)
 {
-  int	i;
-  int	j;
-  char	*prog;
-  char	*arg;
+  t_parse	*tmp;
 
+  tmp = malloc(sizeof(t_parse));
+  if (tmp == NULL)
+    return (0);
+  if (tmp)
+    {
+      tmp->prog = prog;
+      tmp->arg = arg;
+      tmp->next = parse;
+    }
+  return (tmp);
+}
+
+void            affiche(t_parse *parse)
+{
+  t_parse       *tmp;
+
+  tmp = parse;
+  while (tmp != NULL)
+    {
+      my_putstr("Nom du programme : ");
+      my_putstr(tmp->prog);
+      my_putstr("\n");
+      my_putstr("Les arguments: ");
+      my_putstr(tmp->arg);
+      my_putstr("\n\n------------------\n\n");
+      tmp = tmp->next;
+    }
+}
+
+char		*parcours(char *str, t_parse *parse)
+{
+  int		i;
+  int		j;
+  char		*prog;
+  char		*arg;
+  
   i = 0;
   j = 0;
   prog = malloc(sizeof(char));
   arg = malloc(sizeof(char));
-  if (prog == 0 || arg == 0)
-    return (NULL);
+  if (prog == NULL || arg == NULL)
+    return (0);
   while (str[i] != '\0' && str[i] != ':')
     {
       prog[i] = str[i];
@@ -35,21 +69,25 @@ char	*parcours(char *str)
       i = i + 1;
       j = j + 1;
     }
-  my_putstr("\n\nprog = ");
-  my_putstr(prog);
-  my_putstr("\narg = ");
-  my_putstr(arg);
+  parse = add_parse(parse, prog, arg);
+  affiche(parse);
 }
 
-void	libparse(int ac, char **av)
+void		libparse(int ac, char **av)
 {
-  int	i;
+  int		i;
+  t_parse	*parse;
   
   i = 1;
+  parse = NULL;
+  if (ac == 1)
+    my_putstr("SYNTAX: ./liparse -prog:argument");
   while (i < ac)
     {
       if (av[i][0] == '-')
-	parcours(av[i]);
+	parcours(av[i], parse);
+      //      else
+      //my_putstr("SYNTAX: ./liparse -prog:argument");
       i = i + 1;
     }
 }
