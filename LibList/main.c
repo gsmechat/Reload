885f43c82b87eb85ecb744699f694d1cb2ad0905
mechat_g <mechat_g@b3d5a8ed-ae99-4402-9d51-05b63b276f9b>
2016-02-12 11:21:10 +0000

    add liblist

---
+++ LibList/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for  in /home/guillaume/Bachelor - Octobre_2018/TIC/Reload/LibList
+** 
+** Made by MECHAT Guillaume
+** Login   <mechat_g@etna-alternance.net>
+** 
+** Started on  Tue Feb  9 05:05:10 2016 MECHAT Guillaume
+** Last update Tue Feb  9 05:09:34 2016 MECHAT Guillaume
+*/
+
+int	main(int argc, char **argv)
+{
+  return (0);
+}
