4d8150a10cd241752744aee573bf6bcee20cff99
mechat_g <mechat_g@b3d5a8ed-ae99-4402-9d51-05b63b276f9b>
2016-02-20 22:12:23 +0000

    add modfi

--- Reload/mechat_g/LibList/main.c
+++ Reload/mechat_g/LibList/main.c
@@ -5,7 +5,7 @@
 ** Login   <mechat_g@etna-alternance.net>
 ** 
 ** Started on  Mon Feb  8 20:40:04 2016 MECHAT Guillaume
-** Last update Sat Feb 13 00:03:08 2016 MECHAT Guillaume
+** Last update Sat Feb 20 22:51:24 2016 MECHAT Guillaume
 */
 #include "header.h"
 #include <stdlib.h>
@@ -25,14 +25,31 @@ int		main(int argc, char **argv)
   list = addList(list, "mdr");
   list = addList(list, "ptdr");
   list = addList(list, "XD");
+  list = addList(list, "caca");
+  list = addList(list, "pipi");
   int j;
+
+  //  list = deleteList(list, "XD");
+  //printf("ici = %d", destroyList(list));
+  //printf("LOL");
+  j = searchList(list, "coucou");
+  printf("%d", j);
 
   j = searchList(list, "mdr");
   printf("%d", j);
-  list = deleteList(list, "mdr");
-  j = searchList(list, "mdr");
-  printf("%d", j);
+
   j = searchList(list, "ptdr");
   printf("%d", j);
-  return (0);
+
+  j = searchList(list, "XD");
+  printf("%d", j);
+  
+  j = searchList(list, "caca");
+  printf("%d", j);
+
+  j = searchList(list, "pipi");
+  printf("%d", j);
+
+  list = destroyList(list);
+
 }
