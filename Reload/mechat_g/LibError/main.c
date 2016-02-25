3434d6d43f0f75270169d10c7d9e0ada4e93959d
mechat_g <mechat_g@b3d5a8ed-ae99-4402-9d51-05b63b276f9b>
2016-02-25 14:05:31 +0000

    add liberror

---
+++ Reload/mechat_g/LibError/main.c
@@ -0,0 +1,15 @@
+/*
+** main.c for  in /home/guillaume/Bachelor - Octobre_2018/TIC/Reload/LibError
+** 
+** Made by MECHAT Guillaume
+** Login   <mechat_g@etna-alternance.net>
+** 
+** Started on  Thu Feb 25 14:31:40 2016 MECHAT Guillaume
+** Last update Thu Feb 25 14:34:28 2016 MECHAT Guillaume
+*/
+
+int	main(int argc, char **argv)
+{
+  error("log.txt");
+  return (0);
+}
