08b0eb10c8fcba5b807b77827c0a412a0aecd28e
mechat_g <mechat_g@b3d5a8ed-ae99-4402-9d51-05b63b276f9b>
2016-02-25 17:07:09 +0000

    add all

--- LibError/liberror.c
+++ LibError/liberror.c
@@ -5,26 +5,66 @@
 ** Login   <mechat_g@etna-alternance.net>
 ** 
 ** Started on  Thu Feb 25 10:34:48 2016 MECHAT Guillaume
-** Last update Thu Feb 25 15:03:28 2016 MECHAT Guillaume
+** Last update Thu Feb 25 18:06:24 2016 MECHAT Guillaume
 */
 
 #include <errno.h>
 #include <fcntl.h>
-#include <string.h>
 
-void	error(void *data)
+int	open (char *path, int access, int right);
+int my_fputs(char *txt, int file);
+void put_error(char *msg);
+
+int my_error(char *message)
 {
-  char	*buf;
-  int	len;
-  int	i;
-
-  i = 0;
-  buf = perror();
-  len = my_strlen(buf);
-  open("log.txt", O_APPEND);
-  while (i < len)
+  if(message)
     {
-      write(1, &buf[i], i);
-      i = i + 1;
+      perror(message);
+      put_error(message);
+      return (1);
     }
+  return (0);
+}
+
+void put_error(char *message)
+{
+  int file;
+  char *error;
+  
+  file = my_fopen("log.txt", O_APPEND, 0777);
+  if(file == 0)
+    {
+      my_putstr("log file can't be writed");
+      return;
+    }
+  my_fputs(message, file);
+  my_fputs(": ", file);
+  //  error = strerror(errno);
+  //my_fputs(error, file);
+  my_fputs("\n", file);
+  close(file);
+
+}
+
+int	open(char *path, int access, int right)
+{
+
+  int fd;
+  fd = open(path, access|O_CREAT|O_RDWR, right);
+  return (fd);
+}
+
+int my_fputs(char *txt, int file)
+{
+  int n;
+  n = 0;
+  //  if (file < 0)
+    n = write(file, txt, my_strlen(txt));
+
+    //if(n < 0)
+    //{
+      perror("my_fputs()");
+      return (errno);
+      //}
+  return (1);
 }
--- LibError/main.c
+++ LibError/main.c
@@ -5,11 +5,12 @@
 ** Login   <mechat_g@etna-alternance.net>
 ** 
 ** Started on  Thu Feb 25 14:31:40 2016 MECHAT Guillaume
-** Last update Thu Feb 25 14:34:28 2016 MECHAT Guillaume
+** Last update Thu Feb 25 18:04:37 2016 MECHAT Guillaume
 */
 
 int	main(int argc, char **argv)
 {
-  error("log.txt");
+  
+  my_error("No file or directory");
   return (0);
 }
